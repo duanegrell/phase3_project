3b9f952a45847a88de1c766b59034af9c5c7240e
Susan Ahmed <susanahmed617@gmail.com>
2023-03-07 13:32:05 -0500

    added student table

---
+++ lib/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, func
+from sqlalchemy import Table, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///phase3_project.db')
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    grade = Column(Integer())
+    subject = Column(String())
+
+    def __repr__(self):
+        return f'Student(id={self.id} ' + \
+            f'name = {self.name}, ' + \
+            f'grade = {self.grade}' + \
+            f'subject = {self.subject})'
