9a3c727002792a5640d511abd8049d3df8f55820
Susan Ahmed <susanahmed617@gmail.com>
2023-03-08 13:44:48 -0500

    update to seed.py

--- lib/seed.py
+++ lib/seed.py
@@ -1,28 +1,71 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Student, Tutor
+from faker import Faker
+from random import random, randint
 
 
 engine = create_engine('sqlite:///phase3_project.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-session.query(Student).delete()
-session.commit()
+fake = Faker()
+subject_list = ["Math", "Science", "Literature", "History"]
 
-susan = Student(name="Susan Ahmed", grade= 10, subject = "Math")
-matt = Student(name= "Matthew Williams", grade = 11, subject= "Science")
-duane = Student(name="Duane Grell", grade = 12, subject="History")
-session.add_all([susan, matt, duane])
-session.commit()
+def student_name():
+    session.query(Student).delete()
+    session.commit()
 
-session.query(Tutor).delete()
-session.commit()
+    print("Fetching our scholars...")
+    students = [Student(
+        name = fake.name(),
+        grade = randint(9, 12),
+        subject = random.choice(subject_list)
+    ) for i in range(20)]
 
-eleanor = Tutor(name = "Eleanor", specialty = "Math", rate = 200)
-tyler = Tutor(name = "Tyler", specialty = "Science", rate = 250)
-gehrig = Tutor(name = "Gehrig", specialty = "History", rate = 150)
+    session.add_all(student_name)
+    session.commit()
+
+    return students
+
+def tutor_name():
+    session.query(Tutor).delete()
+    session.commit()
+
+    print("Let's find you a tutor...")
+    tutors = [Tutor(
+        name = fake.name(),
+        specialty = random.choice(subject_list),
+        rate = randint(100, 300), 
+    ) for i in range(15)]
+
+    session.add_all(tutors)
+    session.commit()
+
+    return tutors
 
 
-session.add_all([eleanor, tyler, gehrig])
-session.commit()
+
+
+    if __name__ == '__main__':
+        session.query()
+
+# session.query(Student).delete()
+# session.commit()
+
+# susan = Student(name="Susan Ahmed", grade= 10, subject = "Math")
+# matt = Student(name= "Matthew Williams", grade = 11, subject= "Science")
+# duane = Student(name="Duane Grell", grade = 12, subject="History")
+# session.add_all([susan, matt, duane])
+# session.commit()
+
+# session.query(Tutor).delete()
+# session.commit()
+
+# eleanor = Tutor(name = "Eleanor", specialty = "Math", rate = 200)
+# tyler = Tutor(name = "Tyler", specialty = "Science", rate = 250)
+# gehrig = Tutor(name = "Gehrig", specialty = "History", rate = 150)
+
+
+# session.add_all([eleanor, tyler, gehrig])
+# session.commit()
